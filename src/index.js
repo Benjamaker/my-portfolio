0687735bdd48054b69f22331c1335a4d02d24208
Ben Galley <bengalley21@gmail.com>
2020-06-10 13:47:21 +0100

    removed files not needed at this stage

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+class App extends React.component {
+  render() {
+    return(
+      <div className="App">
+        <h1>Hello, React!</h1>
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
